b53efebae4bcd0f20f0e1c4b02725eece9eea21c
Carlos97Baptista <carloshenriquesouzabaptista@gmail.com>
2022-01-22 22:23:00 -0300

    screenshot

--- e2e/Resgate.e2e.js
+++ e2e/Resgate.e2e.js
@@ -8,39 +8,55 @@ describe('Testes', () => {
   });
 
   it('Teste 1 - Clicar em confirmar com mais de um campos a resgatar com valor invalido', async () => {
+
     await waitFor(element(by.text('INVESTIMENTO I'))).toBeVisible().withTimeout(5000);
     await element(by.id('INVESTIMENTO I')).tap();
+
     await waitFor(element(by.id('resgate-details'))).toBeVisible().withTimeout(5000);
+
     await element(by.id('BBAS3')).tap();
     await element(by.id('BBAS3')).typeText('1200000');
+
     await element(by.id('Keyboard')).tap();
+
     await element(by.id('VALE3')).tap();
     await element(by.id('VALE3')).typeText('900000');
+    
     await element(by.id('Keyboard')).tap();
+
     await element(by.id('resgate-details')).scroll(500, 'down')
     await element(by.id('subimit')).tap();
-    await waitFor(element(by.text("DADOS INVÁLIDOS"))).toBeVisible().withTimeout(5000); 
-    await new Promise((r) => setTimeout(r, 5000));
-    // const info = await element(by.id('1'))
-    // await expect(info).toBeVisible();
+    
+    await device.takeScreenshot('Teste_Dados_Invalidos');
+    await expect(element(by.text("DADOS INVÁLIDOS"))).toBeVisible(); 
+
+    
   });
 
   it('Teste 2 - Clicar em confirmar com todos os campos com valor validos', async () => {
+
     await waitFor(element(by.text('INVESTIMENTO I'))).toBeVisible().withTimeout(5000);
     await element(by.id('INVESTIMENTO I')).tap();
+
     await waitFor(element(by.id('resgate-details'))).toBeVisible().withTimeout(5000);
+
     await element(by.id('BBAS3')).tap();
     await element(by.id('BBAS3')).typeText('1000000');
+
     await element(by.id('Keyboard')).tap();
+
     await element(by.id('VALE3')).tap();
     await element(by.id('VALE3')).typeText('700000');
+
     await element(by.id('Keyboard')).tap();
+
     await element(by.id('resgate-details')).scroll(500, 'down')
     await element(by.id('subimit')).tap();
-    await waitFor(element(by.text("RESGATE EFETUADO!"))).toBeVisible().withTimeout(5000); 
-    await new Promise((r) => setTimeout(r, 5000));
-    // const info = await element(by.id('1'))
-    // await expect(info).toBeVisible();
+
+    await device.takeScreenshot('Teste_Dados_Validos');
+    await expect(element(by.text("RESGATE EFETUADO!"))).toBeVisible(); 
+
+    
   });
 
 });
